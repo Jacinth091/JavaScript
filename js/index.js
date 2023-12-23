486a1de486b8cd253b2b627f84a11e3373636caa
Jacinth <jacinthcedricbarral@gmail.com>
2023-12-23 12:12:17 +0800

    Bro Code | JavaScript Full Course

---
+++ js/index.js
@@ -0,0 +1,118 @@
+// Bro Code | JavaScript Full Course
+
+// Basics Of Javascript
+
+// console.log("Hello, World!");
+// console.log("I like Pizza!");
+
+// window.alert(`This is an Alert Window!`);
+// window.confirm(`Do you like pizza?`);
+
+// Single Line Comment
+
+/* Multiple 
+    line 
+    Comments 
+*/
+
+// document.getElementById('myH1').textContent = "Hello, World!";
+// document.getElementById('myP').textContent = "I like Pizza!";
+
+// Variables
+
+// let x; // Declaration
+// x = 324;    // Assignment
+
+    // Data Type = NUMBERS
+
+    // let age = 25;
+    // let price = 10.99;
+    // let gpa = 2.1;
+
+    // console.log(typeof age, typeof price, typeof gpa);
+    
+    // console.log(`You are ${age} years old`);
+    // console.log(`The price is $${price}`);
+    // console.log(`Your GPA is: ${gpa}`);
+
+
+    // Data Type = STRINGS
+
+        // let firstName = "Jacinth";
+        // let favFood = "Mango-Float"
+        // let email= "jacinthcedricbarral123@gmail.com"
+
+        // console.log(typeof firstName);
+        // console.log(`Your name is ${firstName}`);
+        // console.log(`You like ${favFood}`);
+        // console.log(`Your Email is ${email}`);
+
+
+    // Data Type = BOOLEANS 
+
+        // let online = false;
+        // let forSale = true;
+        // let isStudent = true;
+
+
+        // console.log(typeof online);
+        // console.log(`User is Online: ${online}`);
+        // console.log(`Is this car for sale? ${forSale}`);
+        // console.log(`Enrolled:  ${isStudent}`);
+
+
+    // let fullName = "Jacinth Cedric Barral";
+    // let age = 20;
+    // let isStudent = true;
+
+    // document.getElementById('p1').textContent = `My name is: ${fullName}`;
+    // document.getElementById('p2').textContent = `My Age is: ${age} years old`;
+    // document.getElementById('p3').textContent = `Enrolled: ${isStudent}`;
+
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
